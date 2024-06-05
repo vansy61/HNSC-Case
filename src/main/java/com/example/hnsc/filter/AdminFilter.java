cd7a1f24338daacceff7b3e3ebd0314881887c99
vansy61 <49103390+vansy61@users.noreply.github.com>
2024-06-05 13:37:10 +0700

    add login filter

---
+++ src/main/java/com/example/hnsc/filter/AdminFilter.java
@@ -0,0 +1,21 @@
+package com.example.hnsc.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(filterName = "AdminFilter", urlPatterns = {"/admin/*"})
+public class AdminFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpSession session = ((HttpServletRequest) request).getSession();
+        if (session.getAttribute("user_id") == null) {
+            ((HttpServletResponse) response).sendRedirect("/login");
+        } else {
+            chain.doFilter(request, response);
+        }
+    }
+}
